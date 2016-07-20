c2643cab134be337f1a5d28690714ab923e643cd
Shashank Gupta <27392shashankgupta@gmail.com>
2016-07-20 11:27:34 +0530

    train updated

--- train.py
+++ train.py
@@ -1,11 +1,16 @@
 from data_handler import data_handler
-from model import social2vec
+from model import user2vec
 import pdb
 import numpy as np
 #n = data.shape[0]
-data = data_handler("../../trust-aware-recom/data/rating_with_timestamp.mat", "../../trust-aware-recom/data/trust.mat", "../../trust-aware-recom/data/rating_with_timestamp.mat")
+data = data_handler("rating_with_timestamp.mat", "trust.mat", "rating_with_timestamp.mat")
 data.load_matrices()
 n = data.n
+i = data.i
+h = 100
+d = 100
+u2v = user2vec(n, h,d,i)
+u2v.model1()
 
 # Training for batch mode
 def training_batch(batch_size):
@@ -31,9 +36,20 @@ def training_batch(batch_size):
 def training():
     row = np.arange(n)
     np.random.shuffle(row)
+    print "training u-i model"
+
+    for r in row:
+        col = data.T1[r, :].nonzero()[1]
+        np.random.shuffle(col)
+        for c in col:
+            cost = u2v.sgd_ui([[r,c],data.T1[r,c]/float(5)])
+            print cost
+        pdb.set_trace()
+
+    print "training u-u model"
 
 
 
 if __name__ == "__main__":
-    training_batch(16)
+    training()
     pdb.set_trace()
